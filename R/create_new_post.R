b81e972a1f09ce5e90cea6b87f8fba94be21f58d
Larry Zhang <lyzhang10@gmail.com>
2025-12-29 21:14:21 +1300

    update

---
+++ R/create_new_post.R
@@ -0,0 +1,11 @@
+new_post <- function(sub_folder = paste0("post_", Sys.Date()))
+{the_folder <- sprintf("./posts/%s", sub_folder)
+ if(!dir.exists(the_folder)) dir.create(the_folder)
+ 
+ file_from <- "./posts/template.qmd"
+ file_to <- paste0(the_folder, "/index.qmd")
+ file.copy(from = file_from, to = file_to) 
+  
+}
+
+new_post()
